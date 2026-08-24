0a891a4ef729f1a91ef2a53635f163bc2821cbc3
G4o6d3JC <ogougbetrinite@gmail.com>
2026-08-24 01:35:44 +0100

    first commit

---
+++ app-reservation-online/app/Models/Salle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Salle extends Model
+{
+    use SoftDeletes;
+
+   protected $fillable = [ 'nom' , 'description' , 'capacite' , 'status', 'description' , 'prix' , 'image' , 'localisation'];
+
+   public function reservations(): HasMany {
+    return $this->hasMany(Reservation::class);
+   }
+
+   public function images() : BelongsToMany {
+    return $this->belongsToMany(Image::class)->withPivot('modifier_designation') ;
+   }
+
+}
---
+++ app-reservation-online/database/migrations/2026_08_23_235212_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('date_heure_reservation_debut');
+            $table->date('date_fin_reservation');
+            $table->time('durer_reservation');
+            $table->integer('nombre_personne');
+            $table->enum('status' , ['attente' , 'confrimer' , 'rejecter' , 'terminer'])->default('attente');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('salle_id')->references('id')->on('salles')->cascadeOnDelete();
+            $table->timestamp('terminer_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
